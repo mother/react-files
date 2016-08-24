1cd42e712a2ecc4fd7ebec9fc6286447b626f470
Jared Reich <jaredreich@gmail.com>
2016-08-24 10:29:38 -0600

    add multiple files, multiple drops, remove individual

--- src/Files.js
+++ src/Files.js
@@ -17,33 +17,54 @@ class Files extends React.Component {
     }
   }
 
-  onClick() {
-    this.inputElement.click()
+  componentDidMount() {
+    // stuff
   }
 
-  onDrop(e) {
-    e.preventDefault()
+  componentWillUnmount() {
+    // stuff
+  }
+
+  onClick(event) {
+    if (event.target && event.target.className) {
+      switch(event.target.className) {
+        case 'files-dropzone':
+          this.inputElement.click()
+          break
+        case 'files-list-item-remove':
+          this.fileRemove(event.target.id)
+          break
+        default:
+          break
+      }
+    }
+  }
+
+  onDrop(event) {
+    event.preventDefault()
 
     // Collect added files and cast pseudo-array to Array,
     // then return to method
-    const filesAdded = e.dataTransfer ? e.dataTransfer.files : e.target.files
+    const filesAdded = event.dataTransfer ? event.dataTransfer.files : event.target.files
+    let files = []
     for (let i = 0; i < filesAdded.length; i++) {
       let file = filesAdded[i]
-      file.id = this.id++
+      file.id = 'files-list-item-' + this.id++
       file.preview = window.URL.createObjectURL(file)
-      this.state.files.push(file)
+      files.push(file)
     }
-    this.props.onDrop(this.state.files)
+    this.setState({ files: [...this.state.files, ...files] })
   }
 
-  onDragOver(e) {
-    e.preventDefault()
-    e.stopPropagation()
-    return false
+  onDragOver(event) {
+    event.preventDefault()
+    event.stopPropagation()
   }
 
   fileRemove(fileId) {
-    console.log(fileId)
+    this.setState({
+      files: this.state.files.filter(file => file.id !== fileId)
+    })
   }
 
   render() {
@@ -58,19 +79,30 @@ class Files extends React.Component {
 
     return (
       <div
-        className="div"
+        className="files-container"
         onClick={this.onClick}
-        onDrop={this.onDrop}
-        onDragOver={this.onDragOver}
       >
-        {this.props.children}
         <input
           // {...inputProps/* expand user provided inputProps first so inputAttributes override them */}
           {...inputAttributes}
         />
+        <div
+          className="files-dropzone"
+          onDrop={this.onDrop}
+          onDragOver={this.onDragOver}
+        >
+        </div>
+        {this.props.children}
         {
           this.state.files.length > 0
-          ? <div>{this.state.files.map((file) => <img key={file.id} src={file.preview} onClick={this.fileRemove(file.id)} />)}</div>
+          ? <div>{this.state.files.map((file) =>
+              <div className="files-list-item" key={file.id}>
+                <img className="files-list-item-preview" src={file.preview} />
+                <div>filename.png</div>
+                <div>100kB</div>
+                <img id={file.id} className="files-list-item-remove" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaGVpZ2h0PSI0OCIgdmlld0JveD0iMCAwIDQ4IDQ4IiB3aWR0aD0iNDgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTM4IDEyLjgzbC0yLjgzLTIuODMtMTEuMTcgMTEuMTctMTEuMTctMTEuMTctMi44MyAyLjgzIDExLjE3IDExLjE3LTExLjE3IDExLjE3IDIuODMgMi44MyAxMS4xNy0xMS4xNyAxMS4xNyAxMS4xNyAyLjgzLTIuODMtMTEuMTctMTEuMTd6Ii8+PHBhdGggZD0iTTAgMGg0OHY0OGgtNDh6IiBmaWxsPSJub25lIi8+PC9zdmc+" />
+              </div>
+            )}</div>
           : null
         }
       </div>
--- src/index.js
+++ src/index.js
@@ -3,22 +3,18 @@ import ReactDOM from 'react-dom'
 import Files from './Files'
 
 var FilesDemo = React.createClass({
-  getInitialState: function () {
-    return {
-      files: []
-    }
-  },
+  // getInitialState: function () {
+  //   return {
+  //     files: []
+  //   }
+  // },
 
-  onDrop: function (files) {
-    console.log(files)
-    this.setState({
-      files: files
-    })
-  },
-
-  onClick: function () {
-    console.log('click')
-  },
+  // onDrop: function (files) {
+  //   console.log(files)
+  //   this.setState({
+  //     files: files
+  //   })
+  // },
 
   // {this.state.files.length > 0
   //   ? <div>
@@ -29,11 +25,8 @@ var FilesDemo = React.createClass({
 
   render: function () {
     return (
-      <div>
-        <Files className="wow" onDrop={this.onDrop} onClick={this.onClick}>
-          <div>Try dropping some files here, or click to select files.</div>
-        </Files>
-
+      <div className="files">
+        <Files onDrop={this.onDrop} onClick={this.onClick} />
       </div>
     )
   }
