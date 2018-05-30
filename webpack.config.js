15a90062040767c6be529287cff42903f63267fa
Carter Wooten <cw@carterwooten.com>
2018-05-30 10:34:03 -0400

    Temp fix to webpack config for window not defined error

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'index.js',
-      libraryTarget: 'umd'
+      libraryTarget: 'umd',
+      globalObject: 'this'
    },
    externals: {
       react: 'react'
