af7e9d0b71e3f1839a1302a1efc8ea3d10409329
Ajay Sabhaney <ajay@mother.co>
2023-02-03 15:48:01 -0500

    update some dev dependencies

--- demo/server.js
+++ demo/server.js
@@ -12,40 +12,35 @@ const app = express()
 const upload = multer({ dest: path.join(__dirname, '.uploads') })
 
 app.use(webpackDevMiddleware(compiler, {
-   publicPath: webpackConfig.output.publicPath,
-   noInfo: true,
-   quiet: false,
-   historyApiFallback: true,
-   stats: {
-      colors: true
-   }
+   publicPath: webpackConfig.output.publicPath
 }))
 
 app.use(webpackHotMiddleware(compiler, {
-   log: console.log,
+   log: console.log, // eslint-disable-line no-console
    path: '/__webpack_hmr',
    heartbeat: 10 * 1000
 }))
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
-app.get('/', function (req, res, next) {
-   res.sendFile(path.join(__dirname + '/index.html'))
+app.get('/', (req, res, next) => {
+   res.sendFile(path.join(`${__dirname}/index.html`))
 })
 
-app.post('/files', upload.any(), function (req, res, next) {
+app.post('/files', upload.any(), (req, res, next) => {
    if (!req.files) {
       return next(new Error('No files uploaded'))
    }
 
    req.files.forEach((file) => {
-      console.log(file)
+      console.log(file) // eslint-disable-line no-console
       fs.unlinkSync(file.path)
    })
 
    res.status(200).end()
 })
 
-app.listen(process.env.PORT || 8080, function () {
-   console.log(`Starting react-files demo on port 8080`)
+const server = app.listen(process.env.PORT || 8080, () => {
+   // eslint-disable-next-line no-console
+   console.log(`Starting react-files demo on port ${server.address().port}`)
 })
